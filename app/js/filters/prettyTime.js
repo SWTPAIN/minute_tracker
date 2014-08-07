763ec8bc13a1c7d3c46feb55df71bdd5642b5ce2
KaFaiC <choikafai2004@gmail.com>
2014-08-07 20:15:11 +0800

    Fix prettyTime filter

--- app/js/filters/prettyTime.js
+++ app/js/filters/prettyTime.js
@@ -1,10 +1,10 @@
 angular.module('MinuteTracker')
   .filter('prettyTime', function(){
     return function(second){
-      var s = second % 60;
-      var m = (second - s) % (60*60)
-      var h = (second - m - s ) / (60*60)
-      var time = h ? h+'hr'+m+'min'+s+'sec' : m ? m+'min'+s+'sec' : s + 'sec'
+      var h = Math.floor(second / (60*60))
+      var m = Math.floor((second - h*60*60) / 60)
+      var s = (second - h*60*60 - m*60);
+      var time = h ? h+'h '+m+'m '+s+'s ' : m ? m+'m '+s+'s ' : s + 's '
       return time
     }
   })
